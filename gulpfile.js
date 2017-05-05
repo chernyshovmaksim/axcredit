da9c8cb702911b78afbf6f8c8bf6b56869c46e35
chernyshovmaksim <chernyshovmaksim56@gmail.com>
2017-05-05 15:13:23 +0500

    section 6 complete

--- gulpfile.js
+++ gulpfile.js
@@ -2,12 +2,14 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var autoprefixer = require('gulp-autoprefixer');
 var cleanCSS = require('gulp-clean-css');
+var csso = require('gulp-csso');
 
 gulp.task('less', function(){
 	return gulp.src('less/main.less')
 	.pipe(less())
 	.pipe(autoprefixer())
-	.pipe(cleanCSS())
+	.pipe(csso())
+	// .pipe(cleanCSS())
 	.pipe(gulp.dest('css/'));
 });
 gulp.task('watch', function(){
