8bc90360bb0e4b828e18db4b0ecbba7de096b967
chernyshovmaksim <chernyshovmaksim56@gmail.com>
2017-05-06 14:38:00 +0500

    fix adaptive

--- gulpfile.js
+++ gulpfile.js
@@ -2,8 +2,6 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var autoprefixer = require('gulp-autoprefixer');
 var cleanCSS = require('gulp-clean-css');
-var csso = require('gulp-csso');
-var imagemin = require('gulp-imagemin');
 
 gulp.task('less', function(){
 	return gulp.src('less/main.less')
@@ -14,9 +12,4 @@ gulp.task('less', function(){
 });
 gulp.task('watch', function(){
 	gulp.watch(['less/*.less'],['less']);
-});
-gulp.task('image', function(){
-	gulp.src('./img/')
-	.pipe(imagemin())
-	.pipe(gulp.dest('./dest'));
 });
