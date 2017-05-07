d2a15fc64875b43ecaf08579ff8c75859426eae8
chernyshovmaksim <chernyshovmaksim56@gmail.com>
2017-05-07 10:08:20 +0500

    Fix adaptive html

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,7 @@ var gulp = require('gulp');
 var less = require('gulp-less');
 var autoprefixer = require('gulp-autoprefixer');
 var cleanCSS = require('gulp-clean-css');
+var imagemin = require('gulp-imagemin');
 
 gulp.task('less', function(){
 	return gulp.src('less/main.less')
@@ -10,6 +11,13 @@ gulp.task('less', function(){
 	.pipe(cleanCSS({compatibility: 'ie8'}))
 	.pipe(gulp.dest('css/'));
 });
+
+gulp.task('imagemin', function(){
+	gulp.src('img/*')
+	.pipe(imagemin())
+	.pipe(gulp.dest('dest/'));
+})
+
 gulp.task('watch', function(){
 	gulp.watch(['less/*.less'],['less']);
 });
